ca83440b9d5049d808125a103289541a320a68f4
Nilanjana333 <303nilanjana@gmail.com>
2025-02-25 00:16:27 +0530

    Create day9.js

---
+++ day9/Classwork/day9.js
@@ -0,0 +1,47 @@
+{
+   let a=10
+  console.log(a)
+}
+
+let x=10, y="10", z=30
+ console.log(x+=x)
+console.log(typeof(x))
+console.log(typeof(y))
+if(x===+y){
+    console.log("x=y")
+}else{
+    console.log("x!=y")
+ }
+
+function myFunction(x,y){
+    console.log(x+y)
+}
+
+let a1=10, b=10
+
+const myFunction=(a1, b)=>{
+    console.log(a1+b)
+}
+myFunction(a,b)
+console.log(name)
+var name
+console.log(A)
+let A =10
+console.log(a);
+
+let $name = 10;
+console.log($name);
+
+myName();
+console.log(a);
+var a = 10;
+function myName() {
+  console.log(a);
+}
+
+let fname = "sudip";
+let lname = "pradhan";
+console.log(fname, lname);
+let B = 20,
+  c = "30";
+console.log(B + c);
